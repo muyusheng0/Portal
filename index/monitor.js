c664c2262dcafc4b75484e6234df6b035b0862d1
muyusheng <muyusheng0@163.com>
2023-05-27 04:04:37 +0800

    UI接入传感器数值

--- index/monitor.js
+++ index/monitor.js
@@ -35,15 +35,15 @@ var COLOR = {
 	}
 };
 
-function renderLegend(){
-	drawLegend(COLOR.MACHINE.TYPE_A,25,'A机型');
-	drawLegend(COLOR.MACHINE.TYPE_B,50,'B机型');
-	drawLegend(COLOR.MACHINE.TYPE_C,75,'C机型');
-	drawLegend(COLOR.MACHINE.TYPE_D,100,'D机型');
-	drawLegend(COLOR.MACHINE.TYPE_E,125,'E机型');
-	drawLegend(COLOR.MACHINE.TYPE_F,150,'F机型');
-	drawLegend(COLOR.MACHINE.TYPE_G,175,'G机型');
-}
+// function renderLegend(){
+// 	drawLegend(COLOR.MACHINE.TYPE_A,25,'A机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_B,50,'B机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_C,75,'C机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_D,100,'D机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_E,125,'E机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_F,150,'F机型');
+// 	drawLegend(COLOR.MACHINE.TYPE_G,175,'G机型');
+// }
 
 function drawLegend(pointColor,pointY,text){
 	var ctx = $("#layer03_left_01 canvas").get(0).getContext("2d");
@@ -58,18 +58,9 @@ function drawLegend(pointColor,pointY,text){
 
 
 //存储
-function renderLayer03Right(){
-	drawLayer03Right($("#layer03_right_chart01 canvas").get(0),"#027825",0.66,'300');
-	drawLayer03Right($("#layer03_right_chart02 canvas").get(0),"#006DD6",0.52,'100');
-	drawLayer03Right($("#layer03_right_chart03 canvas").get(0),"#238681",0.34,'12');
-	drawLayer03Right($("#layer03_right_chart04 canvas").get(0),"#740278",0.34,'20');
-	drawLayer03Right($("#layer03_right_chart05 canvas").get(0),"#0032d6",0.34,'28');
-	drawLayer03Right($("#layer03_right_chart06 canvas").get(0),"#598623",0.34,'70');
-}
 
-function drawLayer03Right(canvasObj,colorValue,rate,value){
-	var ctx = canvasObj.getContext("2d");
-    
+function drawLayer03Right(ctx,colorValue,rate,value){
+
 	var circle = {
         x : 65,    //圆心的x轴坐标值
         y : 80,    //圆心的y轴坐标值
@@ -77,10 +68,10 @@ function drawLayer03Right(canvasObj,colorValue,rate,value){
     };
 
 	//画扇形
-	//ctx.sector(circle.x,circle.y,circle.r,1.5*Math.PI,(1.5+rate*2)*Math.PI);
-	//ctx.fillStyle = colorValue;
-	//ctx.fill();
-
+	// ctx.sector(circle.x,circle.y,circle.r,1.5*Math.PI,(1.5+rate*2)*Math.PI);
+	// ctx.fillStyle = colorValue;
+	// ctx.fill();
+	ctx.clearRect(0, 0, 130, 150);
 	ctx.beginPath();
 	ctx.arc(circle.x,circle.y,circle.r,0,Math.PI*2)
 	ctx.lineWidth = 10;
@@ -103,57 +94,57 @@ function drawLayer03Right(canvasObj,colorValue,rate,value){
 }
 
 
-function renderChartBar01(){
-	var myChart = echarts.init(document.getElementById("layer03_left_02"));
-		myChart.setOption(
-					 {
-						title : {
-							text: '',
-							subtext: '',
-							x:'center'
-						},
-						tooltip : {
-							trigger: 'item',
-							formatter: "{b} : {c} ({d}%)"
-						},
-						legend: {
-							show:false,
-							x : 'center',
-							y : 'bottom',
-							data:['A机型','B机型','C机型','D机型','E机型','F机型','G机型']
-						},
-						toolbox: {
-						},
-						label:{
-							normal:{
-								show: true, 
-								formatter: "{b} \n{d}%"
-							} 
-						},
-						calculable : true,
-						color:[COLOR.MACHINE.TYPE_A,COLOR.MACHINE.TYPE_B,COLOR.MACHINE.TYPE_C,COLOR.MACHINE.TYPE_D,COLOR.MACHINE.TYPE_E,COLOR.MACHINE.TYPE_F,COLOR.MACHINE.TYPE_G],
-						series : [
-							{
-								name:'',
-								type:'pie',
-								radius : [40, 80],
-								center : ['50%', '50%'],
-								//roseType : 'area',
-								data:[
-									{value:4600, name:'A机型'},
-									{value:4600, name:'B机型'},
-									{value:15600, name:'C机型'},
-									{value:6600, name:'D机型'},
-									{value:5700, name:'E机型'},
-									{value:7600, name:'F机型'},
-									{value:3500, name:'G机型'}
-								]
-							}
-						]
-					}
-		);
-
-}
+// function renderChartBar01(){
+// 	var myChart = echarts.init(document.getElementById("layer03_left_02"));
+// 		myChart.setOption(
+// 					 {
+// 						title : {
+// 							text: '',
+// 							subtext: '',
+// 							x:'center'
+// 						},
+// 						tooltip : {
+// 							trigger: 'item',
+// 							formatter: "{b} : {c} ({d}%)"
+// 						},
+// 						legend: {
+// 							show:false,
+// 							x : 'center',
+// 							y : 'bottom',
+// 							data:['A机型','B机型','C机型','D机型','E机型','F机型','G机型']
+// 						},
+// 						toolbox: {
+// 						},
+// 						label:{
+// 							normal:{
+// 								show: true,
+// 								formatter: "{b} \n{d}%"
+// 							}
+// 						},
+// 						calculable : true,
+// 						color:[COLOR.MACHINE.TYPE_A,COLOR.MACHINE.TYPE_B,COLOR.MACHINE.TYPE_C,COLOR.MACHINE.TYPE_D,COLOR.MACHINE.TYPE_E,COLOR.MACHINE.TYPE_F,COLOR.MACHINE.TYPE_G],
+// 						series : [
+// 							{
+// 								name:'',
+// 								type:'pie',
+// 								radius : [40, 80],
+// 								center : ['50%', '50%'],
+// 								//roseType : 'area',
+// 								data:[
+// 									{value:4600, name:'A机型'},
+// 									{value:4600, name:'B机型'},
+// 									{value:15600, name:'C机型'},
+// 									{value:6600, name:'D机型'},
+// 									{value:5700, name:'E机型'},
+// 									{value:7600, name:'F机型'},
+// 									{value:3500, name:'G机型'}
+// 								]
+// 							}
+// 						]
+// 					}
+// 		);
+//
+// }
 
 /*
 function renderChartBar02(){
@@ -205,228 +196,109 @@ function renderChartBar02(){
 		);
 }*/
 
-function renderLayer04Left(id){
-	var myChart = echarts.init(document.getElementById(id));
-	myChart.setOption(
-		{
-			title: {
-				text: ''
-			},
-			tooltip : {
-				trigger: 'axis'
-			},
-			legend: {
-				data:[]
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				top:'4%',
-				containLabel: true
-			},
-			xAxis :
-			{
-				type : 'category',
-				boundaryGap : false,
-				data : getLatestDays(31),
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-					},
-					rotate:45,
-					interval:2
-				},
-				axisTick:{show:false},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				}
-			},
-			yAxis : 
-			{
-				type : 'value',
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
-				},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				},
-				splitLine:{
-					show:false
-				}
-			},
-			tooltip:{
-				formatter:'{c}',
-				backgroundColor:'#FE8501'
-			},
-			series : [
-				{
-					name:'',
-					type:'line',
-					smooth:true,
-					areaStyle:{
-						normal:{
-							color:new echarts.graphic.LinearGradient(0, 0, 0, 1, [{offset: 0, color: '#026B6F'}, {offset: 1, color: '#012138' }], false),
-							opacity:0.2
-						}
-					},
-					itemStyle : {  
-                            normal : {  
-                                  color:'#009991'
-                            },
-							lineStyle:{
-								normal:{
-								color:'#009895',
-								opacity:1
-							}
-						}
-                    },
-					symbol:'none',
-					data:[100,200,200,500]
-					// data:[48, 52, 45, 46, 89, 120, 110,100,88,96,88,45,78,67,89,103,104,56,45,104,112,132,120,110,89,95,90,89,102,110,110]
-				}
-			]
+function InitChart_TEMP(myChart,init_data){
+	myChart.setOption({
+		title: {
+		text: '温度实时走势图',
+		left:'center',
+			textStyle: {
+			color: 'white'
+			}
+	 },
+		textStyle:{
+			color:'white'
+		},
+	tooltip: {
+		trigger: 'axis',
+			formatter: function (params) {
+			params = params[0];
+			var date = new Date(params.name);
+			return params.value[0] + '  ' + params.value[1];
+		},
+		axisPointer: {
+			animation: false
 		}
-	
+	},
+	xAxis: {
+		type: 'time',
+		splitLine: {
+			show: false
+		}
+	},
+	yAxis: {
+		type: 'value',
+			boundaryGap: [0, '100%'],
+			splitLine: {
+			show: false
+		},
+		name:'温度',
+		min: -40,
+		max: 40,
+		axisLabel: {
+			formatter: '{value} °C'
+		}
+	},
+	series: [
+		{
+			name: 'Fake Data',
+			type: 'line',
+			showSymbol: false,
+			data: init_data
+		}
+	]}
 	);
 }
 
-function renderLayer04Right(){
-	var myChart = echarts.init(document.getElementById("layer04_right_chart"));
+function InitChart_CO2(myChart,init_data){
 	myChart.setOption({
-			title: {
-				text: ''
+		title: {
+			text: '二氧化碳浓度实时走势图',
+			left:'center',
+			textStyle: {
+				color: 'white'
+			}
+		},
+		textStyle:{
+			color:'white',
+		},
+		tooltip: {
+			trigger: 'axis',
+			formatter: function (params) {
+				params = params[0];
+				var date = new Date(params.name);
+				return params.value[0] + '  ' + params.value[1];
 			},
-			tooltip: {
-				trigger: 'axis'
+			axisPointer: {
+				animation: false
+			}
+		},
+		xAxis: {
+			type: 'time',
+			splitLine: {
+				show: false
+			}
+		},
+		yAxis: {
+			type: 'value',
+			boundaryGap: [0, '100%'],
+			splitLine: {
+				show: false
 			},
-			legend: {
-				top:20,
-				right:5,
-				textStyle:{
-					color:'white'
-				},
-				orient:'vertical',
-				data:[
-						{name:'网络',icon:'circle'},
-						{name:'内存',icon:'circle'},
-						{name:'CPU',icon:'circle'}
-					]
-			},
-			grid: {
-				left: '3%',
-				right: '16%',
-				bottom: '3%',
-				top:'3%',
-				containLabel: true
-			},
-			xAxis: {
-				type: 'category',
-				boundaryGap: false,
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
-				},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				},
-				data: get10MinutesScale()
-			},
-			yAxis: {
-				type: 'value',
-				axisTick:{show:false},
-				axisLabel:{
-					textStyle:{
-						color:"white", //刻度颜色
-						fontSize:8  //刻度大小
-						}
-				},
-				axisLine:{
-					show:true,
-					lineStyle:{
-						color: '#0B3148',
-						width: 1,
-						type: 'solid'
-					}
-				},
-				splitLine:{
-					show:false
-				}
-			},
-			series: [
-						{
-							name:'网络',
-							type:'line',
-							itemStyle : {  
-									normal : {  
-									color:'#F3891B'
-								},
-								lineStyle:{
-									normal:{
-									color:'#F3891B',
-									opacity:1
-										}
-								}
-							},  
-							data:[120, 132, 101, 134, 90, 230, 210]
-						},
-						{
-							name:'内存',
-							type:'line',
-							itemStyle : {  
-									normal : {  
-									color:'#006AD4'
-								},
-								lineStyle:{
-									normal:{
-									color:'#F3891B',
-									opacity:1
-										}
-								}
-							},
-							data:[220, 182, 191, 234, 290, 330, 310]
-						},
-						{
-							name:'CPU',
-							type:'line',
-							itemStyle : {  
-									normal : {  
-									color:'#009895'
-								},
-								lineStyle:{
-									normal:{
-									color:'#009895',
-									opacity:1
-										}
-								}
-							},
-							data:[150, 232, 201, 154, 190, 330, 410]
-						}
-					]
-		}	
+			name:'浓度(ppm)',
+			min: 0,
+			max: 1000,
+			axisLabel: {
+				formatter: '{value}'
+			}
+		},
+		series: [
+			{
+				name: 'Fake Data',
+				type: 'line',
+				showSymbol: false,
+				data: init_data,
+			}
+		]}
 	);
 }
 
@@ -455,3 +327,16 @@ function getLatestDays(num)
 	}
 	return returnDays;
 }
+
+function getUTCTime() {
+	let d1 = new Date();
+	let d2 = new Date( d1.getUTCFullYear(), d1.getUTCMonth(), d1.getUTCDate(), d1.getUTCHours(), d1.getUTCMinutes(), d1.getUTCSeconds() );
+	return Date.parse(d2);
+}
+const getFormatTime = (stamp) => {
+	let year = new Date(stamp).getFullYear()
+	let month = new Date(stamp).getMonth() + 1
+	month = month < 10 ? '0' + month : month
+	let date = new Date(stamp).getDate()
+	date = date < 10 ? '0' + date : date
+	return year + '-' + month + '-' + date + ' ' + new Date(stamp).toLocaleTimeString('chinese', {hour12: false})}
